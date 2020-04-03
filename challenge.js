e1f17ec8e6a4c0170f0c97af6bdad750a4a1af2a
jamel brown <60190252+mellyallstate@users.noreply.github.com>
2020-04-03 02:04:51 -0400

    part 1
    questions 1-8

--- challenge.js
+++ challenge.js
@@ -12,25 +12,42 @@ const solarSystem = [
 ];
 
 // 1. Print the array of Jupiter's moons to the console (no for loop, just print the entire array).
-
+        console.log(solarSystem[4].moons)
 
 // 2. Print the name of Neptune's moon "Nereid" to the console.
-
+         
+       console.log(solarSystem[7].moons[1])
 
 // 3. Add a new moon called "Endor" to Venus' moons array.
-
+      solarSystem[1].moons = "Endor";
 
 // 4. Add a Pluto object to the solarSystem array using .push. The object should contain Pluto's name, ringSystem boolean, and moons array (which includes "Charon").
-
+      solarSystem.push({name: "Pluto", ringSystem: false, moons: ["Charon"]});
+      solarSystem[2].diameter = "7917.5";
 
 // 5. Add a new key value pair to the the Earth object: the key should be 'diameter', and the value should be Earth's diameter in miles represented as a string.
+      solarSystem.push({name: "Pluto", ringSystem: false, moons: ["Charon"]});
+      console.log(solarSystem);
+
 
 // 6. Change  Mercury's ringSystem boolean to true.
-
+      solarSystem[0].ringSystem = true;
+      console.log(solarSystem[0]);
 
 // 7. Change Uranus' moon "Umbriel" to "Oberon"
-
+      solarSystem[6].moons[4] = "Oberon";
+      console.log(solarSystem[6]);
 // 8. Iterate through the solarSystem array and print only the objects that have a ringSystem (where ringSystem: true), and ignore the others. You want to make a loop that goes over each item in the array, and include an if statement.
+      for (let i = 0; i < solarSystem.length; i++) {
+	if (solarSystem[i].ringSystem) {
+		console.log(solarSystem[i]);
+	}
+}
+
+
+
+
+
 
 // The Solar System, Part 2
 const system = { 
